8e1b565885a35789b0fd716811d03a04b6c4b15c
vickykumar2507 <surajjha.virat@gmail.com>
2024-07-06 11:46:18 +0530

    updated fix fr chrom

--- src/main/java/com/qa/opencart/factory/OptionsManager.java
+++ src/main/java/com/qa/opencart/factory/OptionsManager.java
@@ -20,6 +20,7 @@ public class OptionsManager {
 
 	public ChromeOptions getChromeOptions() {
 		co = new ChromeOptions();
+		co.addArguments("--remote-allow-origin=*");
 		if(Boolean.parseBoolean(prop.getProperty("headless").trim())) {
 		co.addArguments("--headless");
 		}
